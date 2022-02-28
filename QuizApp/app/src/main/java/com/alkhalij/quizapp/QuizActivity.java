df29d292cadf557b63631d557a1c121031a873ce
OmarInCS <omarincs@gmail.com>
2022-02-28 09:36:35 +0300

    check choices

--- QuizApp/app/src/main/java/com/alkhalij/quizapp/QuizActivity.java
+++ QuizApp/app/src/main/java/com/alkhalij/quizapp/QuizActivity.java
@@ -1,14 +1,17 @@
 package com.alkhalij.quizapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
-
+// 05 99 888 921
 public class QuizActivity extends AppCompatActivity {
 
     private TextView tvQuestion;
@@ -36,8 +39,55 @@ public class QuizActivity extends AppCompatActivity {
         generateQuestion();
         generateChoices();
 
+        for (int i = 0; i < btChoices.length; i++) {
+            int finalI = i;
+            btChoices[i].setOnClickListener(view -> checkChoice(finalI));
+        }
+    }
+
+    private void checkChoice(int idx) {
+        int userChoice = choices.get(idx);
+
+        if (userChoice == x*y) {
+            Toast
+                .makeText(this,
+                        "Correct Answer",
+                        Toast.LENGTH_SHORT)
+                .show();
+            correct++;
+        }
+        else {
+            Toast
+                .makeText(this,
+                        "Wrong Answer",
+                        Toast.LENGTH_SHORT)
+                .show();
+        }
+
+        checkIfCompleted();
+    }
+
+    private void checkIfCompleted() {
+
+        if (numOfQuestions != 5) {
+            generateQuestion();
+            generateChoices();
+        }
+        else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Your score");
+            builder.setMessage("Your score is: " + correct + " out of 5");
+            builder.setNeutralButton("OK", (dialog, i) -> {
+                dialog.cancel();
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            });
+
+            builder.create().show();
+        }
 
     }
+
 
     private void generateChoices() {
         choices = new ArrayList<>();
